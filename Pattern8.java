4916e02d65004f726896b5fbca515c2f974e9bb0
SANTANIL BASAK <santanilbasak@gmail.com>
2019-03-04 21:20:44 +0530

    Add files via upload

---
+++ Pattern8.java
@@ -0,0 +1,27 @@
+
+
+/*
+
+*******  
+ *****   
+  ***    
+   *    
+
+*/
+public class Pattern8 {
+
+	public static void main(String args[])
+	{
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=9;j++)
+			{
+				if(j>=i && j<=8-i)
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+} 
